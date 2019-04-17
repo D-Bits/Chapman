b77eb62fafe22bb8a06205a325307c3ea1a0a6f2
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-04-16 17:26:49 -0700

    updates

--- csv_etl.py
+++ csv_etl.py
@@ -29,7 +29,7 @@ def etl():
     # Seed the users table w/ sample data from csv file
     with open('data.csv', 'r') as data_file:
         reader = csv.reader(data_file)
-        next(reader) # Skip header row
+        next(reader)  # Skip header row
         for row in reader:
             engine.execute( # Parameterize query to avoid SQL injections
                 "INSERT INTO users(email, lname, fname, homeaddress) VALUES(%s, %s, %s, %s)",
