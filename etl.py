ccc64eb81eed79f5e4af5b736937b5e31fff9209
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-10-01 20:35:23 -0700

    Moved legacy code to separate file

--- etl.py
+++ etl.py
@@ -37,17 +37,4 @@ def excel_etl(src, sheet, table):
         input('Error: Incompatible data type! Press enter to exit.')
 
 
-# An older-methodoology, using raw SQL.
-def raw_sql_etl():
 
-    # Seed the users table w/ sample data from csv file
-    with open('employees.csv', 'r') as data_file:
-        reader = csv.reader(data_file)
-        next(reader)  # Skip header row
-        for row in reader:
-            connection.execute( # Parameterize query to avoid SQL injections
-                "INSERT INTO employees(fname, lname, email, street, city) VALUES(%s, %s, %s, %s, %s)",
-                row
-            )
-
-        print('Query executed.')
---
+++ old_etl.py
@@ -0,0 +1,16 @@
+
+
+# An older-methodoology, using raw SQL.
+def raw_sql_etl():
+
+    # Seed the users table w/ sample data from csv file
+    with open('employees.csv', 'r') as data_file:
+        reader = csv.reader(data_file)
+        next(reader)  # Skip header row
+        for row in reader:
+            connection.execute( # Parameterize query to avoid SQL injections
+                "INSERT INTO employees(fname, lname, email, street, city) VALUES(%s, %s, %s, %s, %s)",
+                row
+            )
+
+        print('Query executed.')
