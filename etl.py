9632bbaf7d6548cfcfd54aa17d451f0956f6d45f
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-10-02 22:54:06 -0700

    Misc changes

--- etl.py
+++ etl.py
@@ -9,6 +9,7 @@ from pandas.errors import DtypeWarning, EmptyDataError
 from config import db_connection, db_engine  
 
 
+
 # ETL for CSV files. "src" = csv file to extract from, 
 # "table"= db table to load data into. 
 def csv_etl(src, table):
@@ -16,12 +17,14 @@ def csv_etl(src, table):
     try:        
         df = pd.read_csv(src)
         df.to_sql(table, db_engine, index_label='id', if_exists='append')
-        input('Data has finished loading into the database. Press enter to exit.')
-        
+        input(f'{len(df)} record(s) loaded into the database. Press enter to exit.')
+
+    # Throw exception if data source is empty           
     except EmptyDataError:
         input('Error: No data in data source! Press enter to exit.')
+    # Throw exception if data types are not compatible 
     except DtypeWarning:
-        input('Error: Incompatible data type! Press enter to exit.')
+        input('Error: Incompatible data type(s)! Press enter to exit.')
 
 
 # ETL for Excel work books
@@ -30,11 +33,17 @@ def excel_etl(src, sheet, table):
     try:
         df = pd.read_excel(src, sheet_name=sheet)
         df.to_sql(table, db_engine, index_label='id', if_exists='append')
+        input(f'{len(df)} record(s) loaded into the database. Press enter to exit.')
 
+    # Throw exception if data source is empty
     except EmptyDataError:
         input('Error: No data in data source! Press enter to exit.')
+    # Throw exception if data types are not compatible 
     except DtypeWarning:
         input('Error: Incompatible data type! Press enter to exit.')
 
 
+# ETL for JSON datasets
+def json_etl(src, table):
 
+    pass
