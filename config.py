210d130e2802b1428bf40c8dfdc8880f38a21e50
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-10-02 23:54:00 -0700

    Added API data source, and key.

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ database credentials.
 from os import getenv
 from psycopg2 import connect
 from sqlalchemy import create_engine
+from requests import get
 
 
 # Define credentials using environment vars
@@ -17,3 +18,8 @@ db_connection = connect(database='ETL_Test', host=pghost, user=pguser, password=
 
 # Create a SQLAlchemy engine to write CSV data to the db
 db_engine = create_engine(f"postgresql+psycopg2://{pguser}:{pgpass}@{pghost}:{pgport}/ETL_Test")
+
+# API endpoint and key
+api_key = getenv('MOCKAROO_API_KEY')
+endpoint = get(f'https://my.api.mockaroo.com/super_secret_info.json?key={api_key}')
+api_json = endpoint.json()
