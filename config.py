5d650ea730c6b93893705d22dbef7c271dde6bf7
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-10-18 20:47:05 -0700

    Used env var for API key

--- config.py
+++ config.py
@@ -8,11 +8,17 @@ from sqlalchemy import create_engine
 from requests import get
 
 
-# Define credentials using environment vars
+# Define credentials for local Postgres instance
 pghost = getenv('PG_HOST')
 pguser = getenv('PG_USER')
 pgpass = getenv('PG_PASS')
 pgport = getenv('PG_PORT')
+api_key = getenv('MOCKAROO_API_KEY')
+
+# Define credentials for AWS Postgres instance
+pghost = getenv('AWS_PG_HOST')
+pguser = getenv('AWS_PG_USER')
+pgpass = getenv('AWS_PG_PASS')
 
 db_connection = connect(database='etl_test', host=pghost, user=pguser, password=pgpass)
 
@@ -21,6 +27,6 @@ db_engine = create_engine(f"postgresql+psycopg2://{pguser}:{pgpass}@{pghost}:{pg
 
 # API endpoint and key
 api_key = getenv('MOCKAROO_API_KEY')
-endpoint = get(f'https://my.api.mockaroo.com/super_secret_info.json?key=53c79190')
+endpoint = get(f'https://my.api.mockaroo.com/super_secret_info.json?key={api_key}')
 api_json = endpoint.json()
 
