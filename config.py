4b3014ce7720050c77c5117e02c52027e515f3eb
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-10-20 16:46:36 -0700

    Updated config info

--- config.py
+++ config.py
@@ -19,13 +19,20 @@ aws_pghost = getenv('AWS_PG_HOST')
 aws_pguser = getenv('AWS_PG_USER')
 aws_pgpass = getenv('AWS_PG_PASS')
 
+# Define credentials for AWS SQL Server instance
+aws_msssql_host = getenv('AWS_MSSQL_HOST')
+aws_mssql_user = getenv('AWS_MSSQL_USER')
+aws_mssql_pass = getenv('AWS_MSSSQL_PASS')
+aws_mssql_port = getenv('AWS_MSSSQL_PASS')
+
 local_conn = connect(database='etl_test', host=local_pghost, user=local_pguser, password=local_pgpass)
 aws_conn = connect(database='etl_test', host=local_pghost, user=local_pguser, password=local_pgpass)
 
 # Create a SQLAlchemy engine for the local Postgres instance 
 local_engine = create_engine(f"postgresql+psycopg2://{local_pguser}:{local_pgpass}@{local_pghost}:{pgport}/etl_test")
 # Create a SQLAlchemy engine for the AWS Postgres instance 
-aws_engine = create_engine(f"postgresql+psycopg2://{aws_pguser}:{aws_pgpass}@{aws_pghost}:{pgport}/etl_test")
+aws_pg_engine = create_engine(f"postgresql+psycopg2://{aws_pguser}:{aws_pgpass}@{aws_pghost}:{pgport}/etl_test")
+
 
 # API endpoint and key
 api_key = getenv('MOCKAROO_API_KEY')
