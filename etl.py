d3c30444aacf55732cdd0585acdcdc42738def45
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-10-20 22:07:47 -0700

    Added additonal exception handling to json_etl().

--- etl.py
+++ etl.py
@@ -27,7 +27,7 @@ def csv_etl(src, table):
     try:        
         df = pd.read_csv(src)
         df.to_sql(table, local_pg_engine, index_label='id', dtype=data_types, if_exists='append')
-        input(f"{len(df)} record(s) successfully loaded into '{table}'. Press enter to exit.")
+        input(f'{len(df)} record(s) successfully loaded into "{table}". Press enter to exit.')
 
     # Throw exception if data source is empty           
     except EmptyDataError:
@@ -56,7 +56,7 @@ def excel_etl(src, sheet, table):
     try:
         df = pd.read_excel(src, sheet_name=sheet)
         df.to_sql(table, local_pg_engine, index_label='id', dtype=data_types, if_exists='append')
-        input(f'{len(df)} record(s) successfully loaded into {table}. Press enter to exit.')
+        input(f'{len(df)} record(s) successfully loaded into "{table}". Press enter to exit.')
 
     # Throw exception if data source is empty
     except EmptyDataError:
@@ -89,7 +89,7 @@ def json_etl(table):
 
     try:
         df.to_sql(table, local_pg_engine, dtype=data_types, if_exists='append')
-        input(f"{len(df)} record(s) successfully loaded into '{table}'. Press enter to exit.")
+        input(f'{len(df)} record(s) successfully loaded into "{table}". Press enter to exit.')
 
     # Throw exception if data source is empty
     except EmptyDataError:
@@ -100,6 +100,10 @@ def json_etl(table):
     # Throw exception if there's an HTTP error
     except HTTPError:
         input('HTTP Error. Press enter to exit.')
+    except ContentDecodingError:
+        input('Content decoding error. Press enter to exit.')
+    except ConnectionError:
+        input('Connection Error. Press enter to exit.')
 
 
 # Migrate a db table from a local Postgres instance to an AWS Postgres instance (Not yet working)
@@ -109,7 +113,7 @@ def aws_pg_migration(src_table, target_table):
     try: 
         data_src = pd.read_sql_table(src_table, local_pg_engine)
         data_src.to_sql(target_table, aws_pg_engine, index_label='id', if_exists='append')
-        input(f'{len(data_src)} records were successfully loaded from the local {src_table} table into the AWS {target_table} table. Press enter to exit.')
+        input(f'{len(data_src)} records were successfully loaded from the local "{src_table}" table into the AWS "{target_table}" table. Press enter to exit.')
     # Throw exception if data source is empty           
     except EmptyDataError:
         input('Error: No data in data source! Press enter to exit.')
@@ -125,7 +129,7 @@ def aws_mssql_migration(src_table, target_table):
     try: 
         data_src = pd.read_sql_table(src_table, local_pg_engine)
         data_src.to_sql(target_table, aws_mssql_engine, index_label='id', if_exists='append')
-        input(f'{len(data_src)} records were successfully loaded from the local {src_table} table into the AWS {target_table} table. Press enter to exit.')
+        input(f'{len(data_src)} records were successfully loaded from the local "{src_table}" table into the AWS "{target_table}" table. Press enter to exit.')
     # Throw exception if data source is empty           
     except EmptyDataError:
         input('Error: No data in data source! Press enter to exit.')
