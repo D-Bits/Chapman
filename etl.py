d65fa9a02e3bcc12958531e080803846129b887d
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-10-20 18:30:57 -0700

    Added MSSQL migrations.

--- etl.py
+++ etl.py
@@ -6,7 +6,8 @@ import pandas as pd
 from sqlalchemy.types import VARCHAR, Date
 from pandas.errors import DtypeWarning, EmptyDataError
 from pandas.io.json import json_normalize
-from config import local_pg_conn, local_engine, aws_pg_engine, api_json  
+from config import local_pg_conn, local_pg_engine, aws_pg_engine, aws_mssql_engine, api_json  
+from requests.exceptions import HTTPError, ContentDecodingError, ConnectionError
 
 
 # ETL for CSV files. "src" = csv file to extract from, 
@@ -25,7 +26,7 @@ def csv_etl(src, table):
 
     try:        
         df = pd.read_csv(src)
-        df.to_sql(table, local_engine, index_label='id', dtype=data_types, if_exists='append')
+        df.to_sql(table, local_pg_engine, index_label='id', dtype=data_types, if_exists='append')
         input(f'{len(df)} record(s) successfully loaded into {table}. Press enter to exit.')
 
     # Throw exception if data source is empty           
@@ -54,7 +55,7 @@ def excel_etl(src, sheet, table):
 
     try:
         df = pd.read_excel(src, sheet_name=sheet)
-        df.to_sql(table, local_engine, index_label='id', dtype=data_types, if_exists='append')
+        df.to_sql(table, local_pg_engine, index_label='id', dtype=data_types, if_exists='append')
         input(f'{len(df)} record(s) successfully loaded into {table}. Press enter to exit.')
 
     # Throw exception if data source is empty
@@ -87,7 +88,7 @@ def json_etl(table):
     df = pd.DataFrame(flattened_json)
 
     try:
-        df.to_sql(table, local_engine, dtype=data_types, if_exists='append')
+        df.to_sql(table, local_pg_engine, dtype=data_types, if_exists='append')
         input(f'{len(df)} record(s) successfully loaded into {table}. Press enter to exit.')
 
     # Throw exception if data source is empty
@@ -96,6 +97,9 @@ def json_etl(table):
     # Throw exception if data types are not compatible 
     except DtypeWarning:
         input('Error: Incompatible data type! Press enter to exit.')
+    # Throw exception if there's an HTTP error
+    except HTTPError:
+        input('HTTP Error. Press enter to exit.')
 
 
 # Migrate a db table from a local Postgres instance to an AWS Postgres instance (Not yet working)
@@ -103,8 +107,24 @@ def aws_pg_migration(src_table, target_table):
 
     # Read from the source table, load into target table
     try: 
-        data_src = pd.read_sql_table(src_table, local_engine)
-        data_src.to_sql(target_table, aws_pg_engine, index_label='id', if_exists='replace')
+        data_src = pd.read_sql_table(src_table, local_pg_engine)
+        data_src.to_sql(target_table, aws_pg_engine, index_label='id', if_exists='append')
+        input(f'{len(data_src)} records were successfully loaded from the local {src_table} table into the AWS {target_table} table. Press enter to exit.')
+    # Throw exception if data source is empty           
+    except EmptyDataError:
+        input('Error: No data in data source! Press enter to exit.')
+    # Throw exception if data types are not compatible 
+    except DtypeWarning:
+        input('Error: Incompatible data type! Press enter to exit.')
+
+
+# Migrate a db table from a local Postgres instance to an AWS SQL Server instance (Not yet working)
+def aws_mssql_migration(src_table, target_table):
+
+    # Read from the source table, load into target table
+    try: 
+        data_src = pd.read_sql_table(src_table, local_pg_engine)
+        data_src.to_sql(target_table, aws_mssql_engine, index_label='id', if_exists='append')
         input(f'{len(data_src)} records were successfully loaded from the local {src_table} table into the AWS {target_table} table. Press enter to exit.')
     # Throw exception if data source is empty           
     except EmptyDataError:
