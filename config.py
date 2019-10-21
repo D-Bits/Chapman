412d5df5fcaaa2e0c116c4eade91fe9e3c91af72
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-10-20 17:59:56 -0700

    Added MSSQL configs

--- config.py
+++ config.py
@@ -19,19 +19,21 @@ aws_pghost = getenv('AWS_PG_HOST')
 aws_pguser = getenv('AWS_PG_USER')
 aws_pgpass = getenv('AWS_PG_PASS')
 
-# Define credentials for AWS SQL Server instance
-aws_msssql_host = getenv('AWS_MSSQL_HOST')
-aws_mssql_user = getenv('AWS_MSSQL_USER')
-aws_mssql_pass = getenv('AWS_MSSSQL_PASS')
-aws_mssql_port = getenv('AWS_MSSSQL_PASS')
-
-local_pg_conn = connect(database='etl_test', host=local_pghost, user=local_pguser, password=local_pgpass)
-aws_conn = connect(database='etl_test', host=local_pghost, user=local_pguser, password=local_pgpass)
-
 # Create a SQLAlchemy engine for the local Postgres instance 
 local_pg_engine = create_engine(f"postgresql+psycopg2://{local_pguser}:{local_pgpass}@{local_pghost}:{pgport}/etl_test")
 # Create a SQLAlchemy engine for the AWS Postgres instance 
 aws_pg_engine = create_engine(f"postgresql+psycopg2://{aws_pguser}:{aws_pgpass}@{aws_pghost}:{pgport}/etl_test")
+
+local_pg_conn = connect(database='etl_test', host=local_pghost, user=local_pguser, password=local_pgpass)
+aws_conn = connect(database='etl_test', host=local_pghost, user=local_pguser, password=local_pgpass)
+
+# Define credentials for AWS SQL Server instance
+aws_msssql_host = getenv('AWS_MSSQL_HOST')
+aws_mssql_user = getenv('AWS_MSSQL_USER')
+aws_mssql_pass = getenv('AWS_MSSQL_PASS')
+aws_mssql_port = getenv('AWS_MSSQL_PORT')
+
+aws_mssql_engine = create_engine(f"mssql+pyodbc://{aws_mssql_user}:{aws_mssql_pass}@{aws_msssql_host}:{aws_mssql_port}/etl_test?driver=SQL+Server+Native+Client+10.0") 
 
 
 # API endpoint and key
