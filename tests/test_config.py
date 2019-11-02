6284a527868443b553b5a4abb7f594613281c9f9
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-11-02 11:48:58 -0700

    Modified config tests

---
+++ tests/test_config.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from config import secrets_endpoint, server_listing_endpoint
+
+
+class ConfigTests(TestCase):
+
+    # Test that GET requests to API endpoint(s) always return a 200 response
+    def test_api_endpoints(self):
+
+        self.assertEqual(secrets_endpoint.status_code, 200)
+        self.assertEqual(server_listing_endpoint.status_code, 200)
