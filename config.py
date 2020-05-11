ff25a8a850d917b2d757bb997de4e07ca7683978
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-05-11 13:57:05 -0700

    Updated db settings.

--- config.py
+++ config.py
@@ -12,25 +12,31 @@ from dotenv import load_dotenv
 # Load environment vars from .env file
 load_dotenv()
 
+# Load local PG credentials
+local_host = getenv("LOCAL_PG_HOST")
+local_db = getenv("LOCAL_PG_DB")
+local_user = getenv("LOCAL_PG_USER") 
+pgpass = getenv("LOCAL_PG_PASS")
+
+# For cursor.copy_from()
+local_pg_conn = connect(database=db, user=local_user, password=pgpass, host=local_host)
+
 # Create a SQLAlchemy engine for the local Postgres instance
-local_pg_string = getenv("LOCAL_DB_STRING") 
+local_pg_string = getenv("LOCAL_SQLA_STRING") 
 local_pg_engine = create_engine(local_pg_string)
 # Create a SQLAlchemy engine for the AWS Postgres instance 
-gcp_pg_string = getenv("GCP_DB_STRING") 
+gcp_pg_string = getenv("GCP_SQLA_STRING") 
 gcp_pg_engine = create_engine(gcp_pg_string)
 
 
-# API endpoint(s) and key
-api_key = getenv('MOCKAROO_API_KEY')
-
 # For "super secret" API
-secrets_endpoint = get(f'https://my.api.mockaroo.com/super_secret_info.json?key={api_key}')
+secrets_endpoint = get(getenv("SECRETS_ENDPOINT"))
 secrets_api_json = secrets_endpoint.json()
 
 # For "servers" API
-server_listing_endpoint = get(f'https://my.api.mockaroo.com/servers.json?key={api_key}')
+server_listing_endpoint = get(getenv("SERVERS_ENDPOINT"))
 server_listing_json = server_listing_endpoint.json()
 
 # For "stocks" API
-stocks_endpoint = get(f'https://my.api.mockaroo.com/stocks.json?key={api_key}')
+stocks_endpoint = get(getenv("STOCKS_ENDPOINT"))
 stocks_json = stocks_endpoint.json()
